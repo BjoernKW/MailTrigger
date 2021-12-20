b74d3898c40749a67d1472b637bb8d8a265f2163
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-20 01:18:34 +0100

    Springdoc update

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY .mvn .mvn
 COPY pom.xml .
 COPY src src
 
-RUN ./mvnw install -DskipTests
+RUN ./mvnw install
 RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 
 FROM openjdk:11-jre-slim
--- src/main/java/com/bjoernkw/mailtrigger/OpenApiConfiguration.java
+++ src/main/java/com/bjoernkw/mailtrigger/OpenApiConfiguration.java
@@ -5,10 +5,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
 public class OpenApiConfiguration {
 
     @Bean
